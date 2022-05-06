2d25310d5e941e65db501215bb0bfd5eceb394b7
sunwo0Kim <swk0513@gmail.com>
2022-05-06 16:58:45 +0900

    Test for modifying data

--- vueproject/src/App.vue
+++ vueproject/src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="menu">
+    <a v-for="(a,i) in menus" :key="i">{{a}}</a>
+  </div>
+
+  <div>
+    <h4 v-for="(b,j) in products_object" :key="j">{{ j }}  {{ b }}</h4> 
+    <p> 50 만원 </p>
+  </div>
+  <div>
+    <h4> {{ products[1] }} </h4>
+    <p> {{ anyPrice }} </p>
+  </div>
+  <div>
+    <h4> {{ products[2] }} </h4>
+    <p> {{ anyPrice }} </p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){ // 데이터 바인딩을 하는 이유 : HTML에 하드코딩 해놓으면 나중에 데이터 변경이 어렵다. + 실시간 자동 렌더링 가능
+    return {
+      menus : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+
+      products_object : {
+        '역삼동원룸' : '50만원',
+        '천호동원룸' : '60만원',
+        '마포구원룸' : '70만원',
+      },
+      anyPrice : '가격은아무거나',
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -21,6 +48,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
