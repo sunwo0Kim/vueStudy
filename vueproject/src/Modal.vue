f374e5e06f73981cf7e99214d6dc1c439991b350
sunwo0Kim <swk0513@gmail.com>
2022-05-16 16:58:28 +0900

    Add modal user input

--- vueproject/src/Modal.vue
+++ vueproject/src/Modal.vue
@@ -3,7 +3,10 @@
     <div class="white-bg">
       <img :src="roomdata[clickData].image" class = "room-img">
       <h4 >{{ roomdata[clickData].title }}</h4>
-      <p>{{roomdata[clickData].content}} <br> {{roomdata[clickData].price}}</p>
+      <p>{{roomdata[clickData].content}}</p>
+      <input v-model="month">
+      <!-- <input @input="month = $event.target.value"> -->
+      <p>{{month}}개월 선택함 : {{roomdata[clickData].price * month}}</p>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
@@ -12,6 +15,11 @@
 <script>
 export default {
     name : 'ModalWindow',
+    data(){
+      return {
+        month : '',
+      }
+    },
     props : {
       roomdata : Array,
       modal : Boolean,
