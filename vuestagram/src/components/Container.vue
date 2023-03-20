f6a6337d5810782424b96627c0a112c65dd483cc
sunwoo_ <sunwoo.kim@telusinternational.com>
2023-03-20 17:12:30 +0900

    Long time no see

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :postdata="postdata"/>
+    <!-- <Post />
+    <Post /> -->
+  </div>
+</template>
+
+<script>
+
+import Post from './Post.vue'
+
+export default {
+  name: "App",
+  components: {
+    Post : Post
+  },
+  props : {
+    postdata : Array,
+  }
+};
+</script>
